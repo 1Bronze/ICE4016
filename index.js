63801c0ff0122800a1f9cde0c45a80805396f6d1
1Bronze <khdkkhdd@gmail.com>
2023-09-07 10:47:32 +0900

    [feat] create index.html

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require("express");
-
-const port = "8080";
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
+const port = "8080";
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
   res.send("hello world");
 });
 
-app.listen(port);
-console.log(`server running on ${port}`);
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
